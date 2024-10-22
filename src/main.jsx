27250d2a067cb1b6464d512a5ddde003d8618773
Arturo Ortueta <dev.arturo.ortueta@gmail.com>
2024-10-22 17:28:08 -0500

    React Router added and some routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import './index.css'
 
+//Rutas de la aplicacion
+import ErrorPage from './error-page.jsx';
+import HomePage from './routes/HomePage.jsx';
+import CharacterPage from './routes/CharacterPage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/characters",
+    element: <CharacterPage/>
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/CharacterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharacterPage = () => {
+  return (
+    <div>
+      <h1>Character Page</h1>
+    </div>
+  )
+}
+
+export default CharacterPage
